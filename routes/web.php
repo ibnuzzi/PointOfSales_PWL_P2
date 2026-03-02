0eb54174c9d17edb251eb4a6b77d3006e0e52bfe
izza <muhammadibnzzi@gmail.com>
2026-03-02 20:07:38 +0700

    Praktikum 4 - LevelController dan View dengan DB Facade

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,11 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\SalesController;
+use App\Http\Controllers\LevelController;
 
 Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/level', [LevelController::class, 'index']);
 
 Route::prefix('category')->group(function () {
     Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
@@ -18,3 +21,5 @@ Route::prefix('category')->group(function () {
 Route::get('/user/{id}/name/{name}', [UserController::class, 'show']);
 
 Route::get('/sales', [SalesController::class, 'index']);
+
+
